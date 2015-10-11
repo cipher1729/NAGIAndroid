1cce3458bb9d94a13e17158fc113da25407be760
cipher1729 <cipher1729@gmail.com>
2015-10-11 03:07:27 -0700

    displaying verterinary places nearby

--- app/src/main/java/com/nagirescue/AnimalServicesFragment.java
+++ app/src/main/java/com/nagirescue/AnimalServicesFragment.java
@@ -1,24 +1,101 @@
 package com.nagirescue;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 /**
  * Created by cipher1729 on 10/10/2015.
  */
 public class AnimalServicesFragment extends Fragment {
-    View rootView;
-
+    public static View rootView;
+    public static double latitude=-1;
+    public static double longitude=-1;
+    LocationListener locationListener;
+    LocationManager locationManager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         int layoutId= R.layout.animalservicelayout;
         rootView = inflater.inflate(layoutId, container, false);
-        //setOnClickListeners();
+        //get latitude and longitude
+
+        setOnClickListeners();
         return rootView;
+    }
+
+    private void setOnClickListeners() {
+        locationManager = (LocationManager)(getActivity().getSystemService(Context.LOCATION_SERVICE));
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+
+        //search gps
+        ((Button)rootView.findViewById(R.id.gpsBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+
+                PlacesTask placesTask= new PlacesTask();
+                placesTask.execute(getActivity());
+            }
+        });
+
+
+        //search address
+        ((Button)rootView.findViewById(R.id.addressBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+
+                int PLACE_PICKER_REQUEST = 1;
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+                startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+            }
+        });
+
+
+
+
     }
 
 }
---
+++ app/src/main/java/com/nagirescue/PlacesTask.java
@@ -0,0 +1,165 @@
+package com.nagirescue;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chandanj on 9/24/2015.
+ */
+public class PlacesTask extends AsyncTask<Context, Void, String>{
+
+    Context cxt;
+    @Override
+    protected String doInBackground(Context... context) {
+
+        //store mainactivity context
+        cxt = context[0];
+
+        // For storing data from web service
+        String data = "";
+
+        // Obtain browser key from https://code.google.com/apis/console
+        String key = "key=AIzaSyDRmC_ch44GZvqcw7hj44L0RpyzV0z9ibg";
+
+        String input="";
+
+       /* try {
+           *//* input = "input=" + URLEncoder.encode(place[0], "utf-8");*//*
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }*/
+
+        // place type to be searched
+        String types = "types=geocode";
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Building the parameters to the web service
+        String parameters;
+        if(AnimalServicesFragment.latitude==-1f ||  AnimalServicesFragment.longitude==-1f)
+        {
+            parameters = "location= 33.3061600,-111.8412500&radius=10000&types=veterinary_care&"+ key;
+        }
+        else
+        {
+            parameters= "location="+ AnimalServicesFragment.latitude+","+AnimalServicesFragment.longitude+"&radius=10000&types=veterinary_care&"+ key;
+
+        }
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/"+output+"?"+parameters;
+
+        try{
+            // Fetching the data from we service
+            data = downloadUrl(url);
+        }catch(Exception e){
+            Log.d("Background Task", e.toString());
+        }
+        return data;
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        // Creating ParserTask
+        /*parserTask = new ParserTask();*/
+
+        // Starting Parsing the JSON string returned by Web Service
+       /* parserTask.execute(result);*/
+
+        //parse the string
+        try {
+            JSONObject obj = new JSONObject(result);
+            JSONArray array = obj.getJSONArray("results");
+            List<String> placeList = new ArrayList<String>();
+            //create POJOs for the object
+            for(int i=0;i<array.length();i++)
+            {
+                JSONObject arrObj = array.getJSONObject(i);
+                String name= arrObj.getString("name");
+                /*String vicinity = arrObj.getString("vicinity");
+                JSONObject openNowObj =  arrObj.getJSONObject("opening_hours");
+                String openNow = Boolean.toString(openNowObj.getBoolean("open_now"));*/
+                placeList.add(name);
+            }
+            //update the UI here
+
+            if(placeList.size()==0)
+                placeList.add("No results found");
+            ListAdapter adapter = new ArrayAdapter<String>(cxt,android.R.layout.simple_list_item_1, placeList);
+            ((ListView)AnimalServicesFragment.rootView.findViewById(R.id.placesList)).setVisibility(View.VISIBLE);
+            ((ListView)AnimalServicesFragment.rootView.findViewById(R.id.placesList)).setAdapter(adapter);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /** A method to download json data from url */
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e){
+            Log.d("Exception while downloading url", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+}
